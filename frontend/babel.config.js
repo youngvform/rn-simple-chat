8a2f5552b9ae716057295f97e9874ed18c5bbfd8
youngvform <austinhbk@naver.com>
2020-10-04 22:46:25 +0900

    updated babel config

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  plugins: [
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: false}],
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
